e5ed582e0c5e21395fd24b29e743cf3e5ce1f850
Ragnar-BY <roman.buzuk@gmail.com>
2018-06-11 17:45:55 +0300

    Added method GetRoomsAvailabilityByTime to interface
    Signed-off-by: Ragnar-BY <roman.buzuk@gmail.com>

--- exchange.go
+++ exchange.go
@@ -1,9 +1,14 @@
 package go_exchange
 
-import "github.com/Ragnar-BY/go-exchange/models"
+import (
+	"time"
+
+	"github.com/Ragnar-BY/go-exchange/models"
+)
 
 // Exchange is interface for requests to MS Exchange.
 type Exchange interface {
 	GetRooms(roomlist string) ([]models.Room, error)
 	GetRoomLists() ([]models.RoomList, error)
+	GetRoomsAvailabilityByTime(rooms []models.Room, start time.Time, end time.Time) ([]models.CalendarEventArray, error)
 }
