6f9da20037da46a5e45bd004a227fb0a22f86607
Ragnar-BY <roman.buzuk@gmail.com>
2018-06-12 12:34:07 +0300

    added method to interface
    Signed-off-by: Ragnar-BY <roman.buzuk@gmail.com>

--- exchange.go
+++ exchange.go
@@ -11,4 +11,5 @@ type Exchange interface {
 	GetRooms(roomlist string) ([]models.Room, error)
 	GetRoomLists() ([]models.RoomList, error)
 	GetRoomsAvailabilityByTime(rooms []models.Room, start time.Time, end time.Time) ([]models.CalendarEventArray, error)
+	GetFreeRoomsByTime(rooms []models.Room, start time.Time, end time.Time) ([]models.Room, error)
 }
